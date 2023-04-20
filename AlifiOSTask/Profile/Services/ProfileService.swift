36987067421ce99b3fa665c328348197a848beab
UKhurshed <khurshedumarov777@gmail.com>
2023-04-20 18:45:53 +0500

    ProfileService commit

---
+++ AlifiOSTask/Profile/Services/ProfileService.swift
@@ -0,0 +1,45 @@
+//
+//  ProfileService.swift
+//  AlifiOSTask
+//
+//  Created by Khurshed Umarov on 20.04.2023.
+//
+
+import Foundation
+
+
+class ProfileServiceImpl: ProfileService {
+    func logOut() throws {
+        guard let realm = StorageManager.shared.realm else {
+            throw CustomError.realmInstanceNotExisting
+        }
+        
+        let loggedStatus = realm.objects(LoggedStatus.self)
+        if let loggedUser = loggedStatus.first(where: { $0.status }) {
+            try realm.write {
+                loggedUser.status = false
+            }
+        } else {
+            throw CustomError.userNotExisting
+        }
+        
+    }
+    
+    func deleteAccount() throws {
+        guard let realm = StorageManager.shared.realm else {
+            throw CustomError.realmInstanceNotExisting
+        }
+        
+        let loggedStatus = realm.objects(LoggedStatus.self)
+        let users = realm.objects(User.self)
+        
+        guard let loggedUser = loggedStatus.first(where: { $0.status }), let user = users.first(where: { $0.name == loggedUser.name}) else {
+            throw CustomError.userNotExisting
+        }
+        
+        try realm.write {
+            realm.delete(loggedUser)
+            realm.delete(user)
+        }
+    }
+}
